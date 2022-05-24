b0a04406f5fe614b2eb85792bc43f772f92942f2
George Saint Hilaire <georgesainthilaire@gmail.com>
2022-05-24 12:43:14 -0400

    the scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+const { Modal } = require("bootstrap");
+
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play")
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+});
+
+$(function() {
+    $("#loginButton", "#reserveButton").on('show.bs.modal', function (event) { var button = $(event.relatedTarget)
+        var recipient = button.data('whatever')
+        var modal = $(this)
+        modal.find('.modal-title').text('New message to ' + recipient)
+        modal.find('.modal-body input').val(recipient)
+    })
+});
