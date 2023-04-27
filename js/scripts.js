440b6bfb5058fc25dbb5a646fd8cf18361dcde38
George Saint Hilaire <georgesainthilaire@Georges-iMac.local>
2023-04-27 13:58:43 -0400

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -16,10 +16,11 @@ $(function() {
 });
 
 $(function() {
-    $("#loginButton", "#reserveButton").on('show.bs.modal', function (event) { var button = $(event.relatedTarget)
-        var recipient = button.data('whatever')
-        var modal = $(this)
-        modal.find('.modal-title').text('New message to ' + recipient)
-        modal.find('.modal-body input').val(recipient)
-    })
+    $("#loginButton, #reserveButton").on('show.bs.modal', function (event) { 
+        var button = $(event.relatedTarget);
+        var recipient = button.data('whatever');
+        var modal = $(this);
+        modal.find('.modal-title').text('New message to ' + recipient);
+        modal.find('.modal-body input').val(recipient);
+    });
 });
