76afce0b16522756e77ca96ac7924fbea41fa654
George Saint Hilaire <georgesainthilaire@Georges-iMac.local>
2023-04-27 14:22:06 -0400

    necesary dependencies added

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
-const { Modal } = require("bootstrap");
+import { Modal } from "bootstrap";
 
-$(function() {
-    $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
-        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-            $(".carousel").carousel("pause");
-            $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play")
-        } else {
-            $(".carousel").carousel("cycle");
-            $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause");
-        }
-    });
+$(function () {
+  $(".carousel").carousel({ interval: 2000 });
+  $("#carouselButton").addEventListener('click',function () {
+    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+      $(".carousel").carousel("pause");
+      $("#carouselButton").children("i").removeClass("fa-pause");
+      $("#carouselButton").children("i").addClass("fa-play");
+    } else {
+      $(".carousel").carousel("cycle");
+      $("#carouselButton").children("i").removeClass("fa-play");
+      $("#carouselButton").children("i").addClass("fa-pause");
+    }
+  });
 });
 
-$(function() {
-    $("#loginButton, #reserveButton").on('show.bs.modal', function (event) { 
-        var button = $(event.relatedTarget);
-        var recipient = button.data('whatever');
-        var modal = $(this);
-        modal.find('.modal-title').text('New message to ' + recipient);
-        modal.find('.modal-body input').val(recipient);
-    });
+$(function () {
+  $("#loginButton, #reserveButton").on("show.bs.modal", function (event) {
+    var button = $(event.relatedTarget);
+    var recipient = button.data("whatever");
+    var modal = $(this);
+    modal.find(".modal-title").text("New message to " + recipient);
+    modal.find(".modal-body input").val(recipient);
+  });
 });
